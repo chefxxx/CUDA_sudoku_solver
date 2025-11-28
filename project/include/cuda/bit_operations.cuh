73fa8f84de6357708fb1540de6b9e8557bb0943d
chefxxx <mateusz.mm100@gmail.com>
2025-11-28 10:45:52 +0100

    Add bit manipulations funcs

--- project/include/cuda/bit_operations.cuh
+++ project/include/cuda/bit_operations.cuh
@@ -5,6 +5,7 @@
 #ifndef SUDOKU_BIT_OPERATIONS_H
 #define SUDOKU_BIT_OPERATIONS_H
 
+#include <bit>
 #include "board_defines.h"
 
 constexpr CONSTRAINTS_TYPE MIN_LSB = 1u;
@@ -14,5 +15,23 @@ __device__ __host__ __forceinline__ void setBitAtIdx(CONSTRAINTS_TYPE &a, const 
 
 // this func is used for checking bits
 __device__ __host__ __forceinline__ bool checkBitAtIdx(const CONSTRAINTS_TYPE a, const uint32_t idx) { return a & (MIN_LSB << idx); }
+
+// this func returns index of lsb index, 0-based
+__device__ __host__ __forceinline__ int getLsb(const CONSTRAINTS_TYPE a)
+{
+#if defined(__CUDA_ARCH__)
+    return __ffs(a) - 1;
+#else
+    return std::countr_zero(a);
+#endif
+}
+
+// retrieves lsb index and resets lsb
+__device__ __host__ __forceinline__ int popLsb(CONSTRAINTS_TYPE &a)
+{
+    const int idx = getLsb(a);
+    a &= a - 1;
+    return idx;
+}
 
 #endif // SUDOKU_BIT_OPERATIONS_H
